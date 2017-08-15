20fbc442b84f7f136279bd80e784002e0268897b
Bill Pfeiffer <bill.pfeiffer@gmail.com>
2017-08-15 11:23:56 -0400

    latest

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,5 +1,5 @@
-import {Component, HostBinding, OnInit} from '@angular/core';
-import {Article} from "./article.model";
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import {Article} from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -9,13 +9,9 @@ import {Article} from "./article.model";
 export class ArticleComponent implements OnInit {
 
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  @Input() article: Article;
 
   constructor() {
-    this.article = new Article(
-      'Angular 2',
-      'http://angular.io',
-      10);
   }
 
   ngOnInit() {
